4cdfa909962f033348c53804f8d6687b38ccab4a
Letsgo-Umair <umair1styearengin123@gmail.com>
2025-08-29 15:54:26 +0500

    both project created

---
+++ my-packaged-app/src/my_packaged_app/main.py
@@ -0,0 +1,4 @@
+print("Top-level hello from main.py!")  
+
+def main() -> None:
+    print("Hello from my-simple-app! — Umair Arshad, PIAICno: 255935")
---
+++ my-simple-app/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from my-simple-app! — Umair Arshad, PIAICno: 255935")
+
+
+if __name__ == "__main__":
+    main()
